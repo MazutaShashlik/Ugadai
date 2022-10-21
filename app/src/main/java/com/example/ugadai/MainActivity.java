8192a43f7781f7053f23803c61c367e2440b6b38
Кузнецов Данил <kuznecovdanil@iMac-Mac005-7.local>
2022-10-21 11:24:47 +0900

    Pract4

---
+++ app/src/main/java/com/example/ugadai/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.ugadai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.*;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //
+    TextView tvInfo;
+    EditText etInput;
+    Button bControl;
+
+    //
+    int guess;
+    boolean gameFinished;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //
+        tvInfo = (TextView)findViewById(R.id.textView);
+        etInput = (EditText)findViewById(R.id.editTextNumber);
+        bControl = (Button) findViewById(R.id.button);
+        //
+        guess = (int)(Math.random()*100);
+        gameFinished = false;
+
+    }
+
+    public void onClick(View v){
+        if (!gameFinished){
+            int inp = Integer.parseInt(etInput.getText().toString());
+            if (inp > guess)
+                tvInfo.setText(getResources().getString(R.string.ahead));
+            if (inp < guess)
+                tvInfo.setText("Недолет!");
+            if (inp == guess)
+            {
+                tvInfo.setText("В яблочко");
+                bControl.setText(getResources().getString(R.string.play_more));
+                gameFinished = true;
+            }
+        }
+        else
+        {
+            guess = (int)(Math.random()*100);
+            bControl.setText("Ввести значение");
+            tvInfo.setText(getResources().getString(R.string.try_to_guess));
+            gameFinished = false;
+        }
+    }
+}
